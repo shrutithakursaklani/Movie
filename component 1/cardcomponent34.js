60487173b50f53b4e06a329a384c9245de58d8ba
shrutithakursaklani <shrutithakursaklani@gmail.com>
2024-09-12 22:13:35 +0530

    Add files via upload

---
+++ component 1/cardcomponent34.js
@@ -0,0 +1,138 @@
+import Slider from "react-slick";
+
+function Cardcomponent34(){
+    
+        var settings = {
+          dots: false,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 7,
+          spaceBetween: 50,
+          slidesToScroll: 4,
+          initialSlide: 0,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        };
+    return(
+        <div className="w-100 gap-5 m-auto">
+            <div className="pe-5 ps-5 container-fluid">
+              <div className="row">
+                <div className="col-lg-6"> <h4 className="mt-4 text-light">
+                   <a href="#" style={{textDecoration:'none', color:'white'}}>K-Dramaland</a> </h4> </div>
+                <div className="col-lg-6"> <h6 className="mt-4 text-light text-end">
+                 <a href="#" style={{textDecoration:'none', color:'white'}}>View All </a></h6> </div>
+              </div>
+           
+            <Slider {...settings}>
+            {data.map((d)=>(
+               <div className=" inner" style={{height:"500px"}}> 
+                 <div className="rounded  "> 
+                    < img src={d.img} style={{height:"450px",width:"100%"}}></img>
+                </div>
+                <div> 
+                  <p className="text-white">{d.namee}</p>
+                  <p className="text-white">{d.review}</p>
+                </div>
+                {/* <div className="outer mt-5 ">
+                 
+              <div>
+              < img src="m9.webp" style={{height:"100px", width:"70%",alignContent:"center"}}></img>
+              </div>
+
+              <div>
+              <button type="button" class="btn btn-lg btn-secondary" ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+              </svg> Watch Now</button>
+
+              <button type="button" class="btn btn-secondary btn-lg" ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+              </svg></button>
+             </div>
+                </div> */}
+                </div>
+                
+            ))}
+            </Slider>
+            </div>
+        </div>
+    )
+}
+const data=[
+    {
+        img: `images/kd1.webp`,
+       
+       
+    },
+    {
+      
+        img: `images/kd2.webp`,
+
+    },
+    {
+      
+        img: `images/kd3.webp`,
+
+    },
+    {
+       
+        img: `images/kd4.webp`,
+
+    },
+    {
+      
+        img: `images/kd5.webp`,
+
+    },
+    {
+      
+        img: `images/kd6.webp`,
+
+    },
+    {
+      
+        img: `images/kd7.webp`,
+
+    },
+    {
+      
+        img: `images/kd8.webp`,
+
+    },
+    {
+      
+        img: `images/kd9.webp`,
+
+    },
+    {
+      
+        img: `images/kd10.webp`,
+
+    },
+   
+ 
+
+]
+
+export default Cardcomponent34;
---
+++ index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+import Nav from './component/Nav';
+import Card from './component/card';
+ import  Cardcomponent from './component 1/cardcomponent';
+ import Cardcomponent1 from './component 1/cardcomponent1';
+ import Cardcomponent2 from './component 1/cardcomponent2';
+ import Cardcomponent3 from './component 1/cardcomponent3';
+import Cardcomponent4 from './component 1/cardcomponent4';
+import Cardcomponent5 from './component 1/cardcomponent5';
+import Cardcomponent6 from './component 1/cardcopmonent6';
+import Cardcomponent7 from './component 1/cardcomponent7';
+import Cardcomponent8 from './component 1/cardcomponent8';
+import Cardcomponent9 from './component 1/cardcomponent9';
+import Cardcomponent10 from './component 1/cardcomponent10';
+import Cardcomponent11 from './component 1/cardcomponent11';
+import Cardcomponent12 from './component 1/cardcomponent12';
+import Cardcomponent13 from './component 1/cardcomponent13';
+import Cardcomponent14 from './component 1/cardcopmonent14';
+import Cardcomponent15 from './component 1/cardcomponent15';
+import Cardcomponent16 from './component 1/cardcomponent16';
+import Cardcomponent17 from './component 1/cardcomponent17';
+import Cardcomponent18 from './component 1/cardcomponent18';
+import Cardcomponent19 from './component 1/cardcomponent19';
+import Cardcomponent20 from './component 1/cardcomponent20';
+import Cardcomponent21 from './component 1/cardcomponent21';
+import Cardcomponent22 from './component 1/cardcomponent22';
+import Cardcomponent23 from './component 1/cardcomponent23';
+import Cardcomponent24 from './component 1/cardcomponent24';
+import Cardcomponent25 from './component 1/cardcomponent25';
+import Cardcomponent26 from './component 1/cardcomponent26';
+import Cardcomponent27 from './component 1/cardcomponent27';
+import Cardcomponent28 from './component 1/cardcomponent28';
+import Cardcomponent29 from './component 1/cardcomponent29';
+import Cardcomponent30 from './component 1/cardcomponent30';
+import Cardcomponent31 from './component 1/cardcomponent31';
+import Cardcomponent32 from './component 1/cardcomponent32';
+import Cardcomponent33 from './component 1/cardcomponent33';
+import Cardcomponent34 from './component 1/cardcomponent34';
+import Cardcomponent35 from './component 1/cardcomponent35';
+import Footer from './component/Footer';
+
+const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+root1.render(
+ <>
+<Nav/>
+
+ </>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <>
+  <Card />
+  <Cardcomponent/>
+  <Cardcomponent1 />
+  <Cardcomponent2 />
+  <Cardcomponent3 />
+  <Cardcomponent4 />
+  <Cardcomponent5 />
+  <Cardcomponent6 />
+  <Cardcomponent7 />
+  <Cardcomponent8 />
+  <Cardcomponent9 />
+  <Cardcomponent10 />
+  <Cardcomponent11 />
+  <Cardcomponent12 />
+  <Cardcomponent13 />
+  <Cardcomponent14 />
+  <Cardcomponent15 />
+  <Cardcomponent16 />
+  <Cardcomponent17 />
+  <Cardcomponent18 />
+  <Cardcomponent19 />
+  <Cardcomponent20 />
+  <Cardcomponent21 />
+  <Cardcomponent22 />
+  <Cardcomponent23 />
+  <Cardcomponent24 />
+  <Cardcomponent25 /> 
+  <Cardcomponent26 />
+  <Cardcomponent27 />
+  <Cardcomponent28 />
+  <Cardcomponent29 />
+  <Cardcomponent30 />
+  <Cardcomponent31 />
+  <Cardcomponent32 />
+  <Cardcomponent33 />
+  <Cardcomponent34 />
+  <Cardcomponent35 />
+  </>
+ );
+
+
+ const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+ root2.render(
+  <>
+ <Footer/>
+ 
+  </>);
